b0c2a485230db08afb20b5974cc150e320e36b1e
raymond-ndibe <rndibe@wikimedia.org>
2025-05-20 05:24:48 +0100

    [components-api.openapi] add an openapi spec endpoint
    Signed-off-by: raymond-ndibe <rndibe@wikimedia.org>

--- components/api/base_router.py
+++ components/api/base_router.py
@@ -1,6 +1,17 @@
+from pathlib import Path
+from typing import Any
+
+import yaml
 from fastapi import APIRouter
 
-from components.models.api_models import HealthState, HealthzResponse
+from components.models.api_models import (
+    HealthState,
+    HealthzResponse,
+    OpenAPISpecResponse,
+)
+
+CURDIR = Path(__file__).parent.absolute()
+OPENAPI_YAML_PATH = f"{CURDIR.parent}/openapi/openapi.yaml"
 
 router = APIRouter()
 
@@ -9,3 +20,11 @@ router = APIRouter()
 def healthz() -> HealthzResponse:
     # TODO: do some actual checks
     return HealthzResponse(data=HealthState(status="OK"))
+
+
+@router.get("/openapi.json")
+def openapi() -> OpenAPISpecResponse:
+    with open(OPENAPI_YAML_PATH, "r") as yaml_file:
+        openapi_definition: dict[str, Any] = yaml.safe_load(yaml_file)
+
+    return OpenAPISpecResponse(data=openapi_definition)
